c74fffcdb34217a936e6176e740a14e524714380
hmshp <hyemisonne@gmail.com>
2022-07-13 00:06:22 +0900

    commit

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -14,6 +14,17 @@ const ShoeGrid = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  @media (max-width: 1500px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  @media (max-width: 1200px) {
+    grid-template-columns: minmax(0, 1fr);
+  }
+
+  gap: 16px;
+`;
 
 export default ShoeGrid;
